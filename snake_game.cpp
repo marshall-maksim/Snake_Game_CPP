56681255456b4a660fd4d058420a9bafe5ec48a0
marshall-maksim <marshall-maksim@github.com>
2023-06-17 13:47:36 +0300

    Settings project

--- snake_game.cpp
+++ snake_game.cpp
@@ -1,1 +1,12 @@
-#include <iostream>
+#include <glut.h> //подключаем заголовочный файл glut.h
+ 
+int main(int argc, char **argv) {
+ 
+	// Инициализация GLUT
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowPosition(100,100);
+	glutInitWindowSize(400,400);
+	glutCreateWindow("Урок 1");
+	return 1;
+}
